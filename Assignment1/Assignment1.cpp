5c253d248cfe23ba56a1750919845fecaabc8ddf
jav76 <unconfigured@null.spigotmc.org>
2020-08-31 23:04:50 -0400

    Menu with user input works.

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -6,8 +6,6 @@ using std::cin;
 using std::endl;
 using std::string; 
 
-char getImplementationOption(void);
-
 struct cityData
 {
     string cityName;
@@ -15,26 +13,77 @@ struct cityData
     float yPos;
 };
 
+const int ARRAYSIZE = 1000;
+char getImplementationOption(void);
+int getOperationOption(void);
+string getCityName(void);
+float getXPos(void);
+float getYPos(void);
+void insertListRecord(cityData cityList[], string cityName, float xPos, float yPos, int& numCities);
+
+
+
+class node
+{
+    public:
+        string cityName = "";
+        float xPos = -1;
+        float yPos = -1;
+        node* next = nullptr;
+};
+
 int main()
 {
-    cityData citiesList[1000];
-    while (true)
+    cityData citiesList[ARRAYSIZE];
+    int numCities = 0;
+    node* head = new node();
+    char implementation;
+    int operation;
+    bool exit = false;
+    while (exit == false)
     {
-        getImplementationOption();
+        implementation = getImplementationOption();
+        operation = getOperationOption();
+        switch (implementation)
+        {
+            case 'A':
+                switch (operation)
+                {
+                    case 1:
+                        // do some shit idk
+                        cout << "I forget so much of this shit";
+                        break;
+
+                    case 8:
+                        exit = true; // shut this bitch down
+                        break;
+                }
+                break;
+            case 'B':
+                switch (operation)
+                {
+                    case 1:
+                        // do some other shit
+                        cout << "I'm just putting this here so this mfer builds";
+                        break;
+                    case 8:
+                        exit = true;
+                        break;
+                }
+                break;
+        }
     }
-
-
-
-
+    return 0;
 }
 
 
 char getImplementationOption(void)
 {
     char userOption;
-    cout << "Select an implementation option:" << endl;
+    cout << "Implementation options:" << endl;
     cout << "A. Use an array-based list implementation" << endl;
     cout << "B. Use a linked-list based implementation" << endl;
+    cout << endl << "Enter your implementation option: ";
     cin >> userOption;
     userOption = toupper(userOption);
     while (cin.fail() || (userOption != 'A' && userOption != 'B'))
@@ -42,17 +91,90 @@ char getImplementationOption(void)
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         cout << endl << "Invalid option entered. Please try again." << endl << endl;
-        cout << "A. Use an array-based list implementation" << endl;
-        cout << "B. Use a linked-list based implementation" << endl;
+        cout << "Enter your implementation option: ";
         cin >> userOption;
         userOption = toupper(userOption);
     }
-    cout << "User selected option " << userOption << endl;
     return userOption;
 
 }
 
-void insertRecord(char implementation, &citiesList, )
+int getOperationOption(void)
 {
+    int userOption;
+    cout << endl << "Operation options:" << endl;
+    cout << "1. Insert a record" << endl;
+    cout << "2. Search for a record by name" << endl;
+    cout << "3. Search for a record by coordinate" << endl;
+    cout << "4. Delete a record by name" << endl;
+    cout << "5. Delete a record by coordinate" << endl;
+    cout << "6. Print record within a given distance of specified location" << endl;
+    cout << "7. Print all record" << endl;
+    cout << "8. Exit" << endl;
+
+    cout << endl << "Enter your operation option: ";
+    cin >> userOption;
+    while (cin.fail() || userOption > 8 || userOption < 1)
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cout << endl << "Invalid option entered. Please try again. " << endl << endl;
+        cout << "Enter your implementation option: ";
+        cin >> userOption;
+    }
+    return userOption;
+}
+
+string getCityName(void)
+{
+    cout << endl << "Enter name of the city: ";
+    string cityName;
+    cin >> cityName;
+    while (cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cout << endl << "Invalid option entered. Please try again." << endl;
+        cout << "Enter name of the city: ";
+        cin >> cityName;
+    }
+    return cityName;
+}
+
+float getXPos(void)
+{
+    cout << endl << "Enter x coordinate of the city: ";
+    float xPos;
+    cin >> xPos;
+    while (cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cout << endl << "Invalid option entered. Please try again." << endl;
+        cout << "Enter x coordinate of the city: ";
+        cin >> xPos;
+    }
+    return xPos;
+}
+
+float getYPos(void)
+{
+    cout << endl << "Enter y coordinate of the city: ";
+    float yPos;
+    cin >> yPos;
+    while (cin.fail())
+    {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        cout << endl << "Invalid option entered. Please try again." << endl;
+        cout << "Enter y coordinate of the city: ";
+        cin >> yPos;
+    }
+    return yPos;
+}
+
+void insertListRecord(cityData cityList[], string cityName, float xPos, float yPos, int& numCities)
+{
+
 
 }
